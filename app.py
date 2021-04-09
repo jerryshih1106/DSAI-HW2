89aa5027e0950a2a00a98aeb75e08b5160ee172a
jerryshih1106 <66662065+jerryshih1106@users.noreply.github.com>
2021-04-10 01:28:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,15 +24,19 @@ def denormalize(train):
     return denorm
 def buildModel(shape):
     model = Sequential()
-    # model.add(GRU(30,input_length=shape[1], input_dim=shape[2],return_sequences=True,bias_regularizer=regularizers.l2(1e-4)))
-    # model.add(GRU(30,input_length=shape[1], input_dim=shape[2],return_sequences=True))
+    # model.add(GRU(50,input_length=shape[1], input_dim=shape[2],return_sequences=True))
+    model.add(GRU(30,input_length=shape[1], input_dim=shape[2],return_sequences=True))
+    # model.add(GRU(5,input_length=shape[1], input_dim=shape[2]))
     model.add(Dropout(0.1))
+    
     model.add(GRU(50,return_sequences=True))
     model.add(Dropout(0.1))
     model.add(GRU(70,return_sequences=True))
     model.add(Dropout(0.1))
     model.add(GRU(100))
     model.add(Dropout(0.1))
+    # model.add(GRU(5))
+    # model.add(Dropout(0.1))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
     return model
@@ -43,17 +47,8 @@ def buildTrain(train, past=30, future=1):
          X_train.append(np.array(train.iloc[i:i+past]))
          Y_train.append(np.array(train.iloc[i+past:i+past+future][0]))
          if i == train.shape[0]-future-past-1:
-             last_dataX.append(train.iloc[i+1:i+past+future])
+             last_dataX.append(np.array(train.iloc[i+1:i+past+1]))
      return np.array(X_train), np.array(Y_train), np.array(last_dataX)
-
-def buildTestX(test):
-    x_test = []
-    x_test.append(np.array(test.iloc[200:230]))
-    return np.array(x_test)
-def buildTestY(test):
-    y_test = []
-    y_test.append(np.array(test.iloc[230:231][0]))
-    return np.array(y_test)
 def shuffle(X,Y):
     np.random.seed(10)
     randomList = np.arange(X.shape[0])
@@ -83,7 +78,9 @@ if __name__ == '__main__':
                         help='output file name')
     args = parser.parse_args()
     #============================訓練=========================== 
-    train = readCSV(args.training)
+    # train = readCSV(args.training)
+    # train = readCSV("training.csv")
+    train = pd.read_csv("training.csv",header=None)
     train_norm = normalize(train)
     X_train, Y_train ,Last_dataX= buildTrain(train_norm,30,1)
     X_train, Y_train = shuffle(X_train, Y_train)
@@ -99,14 +96,14 @@ if __name__ == '__main__':
     y_test = []
     hat_action = []
     have = 0
-    
     y_pre = []
+    gap = 0.5#兩天股票差距
     #=============================ACTION==============================
     for i in range(len(testing.iloc[:,0])):
         print(hat_action)
-        y_test.append(testing.iloc[i][0])
+        if have == -1:#若前一天要求今天賣空 則紀錄今天的股價
+            money = testing.iloc[i][0]
         # pred = pd.DataFrame(pred)
-        
         train = train.append(testing.iloc[i])#將今天資料加入training data
         train_norn = normalize(train)
         X_train,Y_train,Last_dataX = buildTrain(train_norn,30,1)#提出最後三十筆，包含新的第一天
@@ -114,48 +111,62 @@ if __name__ == '__main__':
         pred = pd.DataFrame(pred)
         a = denormalize(pred)
         y_pre.append(a.iloc[0][0])
-        print(np.sqrt((a-y_test[i])**2))
+        y_test.append(testing.iloc[i][0])
+        # print(np.sqrt((a-y_test[i])**2))
         if i < len(testing.iloc[:,0])-1:
+            # y_test.append(testing.iloc[i+1][0])
             if a[0][0]>testing.iloc[i][0]:#明天>今天
                 if have == 1:
-                    hat_action.append(0)
-                    have = 1
-                    continue
+                    if a[0][0]-testing.iloc[i][0] >= gap:
+                        hat_action.append(-1)
+                        have = 0
+                        continue
+                    if a[0][0]-testing.iloc[i][0] < gap:
+                        hat_action.append(0)
+                        have = 1
+                        continue
                 if have == 0:
-                    hat_action.append(-1)
-                    have = -1
-                    continue
+                    if a[0][0]-testing.iloc[i][0] >= gap:
+                        hat_action.append(-1)
+                        have = -1
+                        continue
+                    if a[0][0]-testing.iloc[i][0] < gap:
+                        hat_action.append(0)
+                        have = 0                    
                 if have == -1:
                     hat_action.append(0)
-                    have = -1
+                    have = 0  
                     continue
-            if a[0][0]<testing.iloc[i][0]:
+            if a[0][0]<testing.iloc[i][0]:#今天>明天
                 if have == 1:
-                    hat_action.append(-1)
-                    have = 0
-                    continue
+                    if testing.iloc[i][0]-a[0][0] >= gap:
+                        hat_action.append(-1)
+                        have = 0
+                        continue
+                    if testing.iloc[i][0]-a[0][0] < gap:
+                        hat_action.append(0)
+                        have = 1
+                        continue
                 if have == 0:
-                    # print("fuck")
-                    hat_action.append(1)
-                    # print(type(hat_action))
-                    have = 1
-                    continue
+                    if testing.iloc[i][0]-a[0][0] >= gap:
+                        hat_action.append(1)
+                        have = 1
+                        continue
+                    if testing.iloc[i][0]-a[0][0] < gap:
+                        hat_action.append(0)
+                        have = 0
+                        continue
                 if have == -1:
-                    hat_action.append(1)
-                    have = 0
-                    continue
+                    if money < a: #若賣空價格小於明天預測開盤，不動作
+                        hat_action.append(0)
+                        continue
+                    if money > a: #若賣空價格大於明天預測開盤，則買進賺價差
+                        hat_action.append(1)
+                        have = 0
+                        continue
             if a[0][0] == testing.iloc[i][0]:
                 hat_action.append(0)
-                have = have
                 continue
-#     plt.xlabel('20 days', fontsize = 16)  
-# # 繪圖並設定線條顏色、寬度、圖例
-#     line1, = plt.plot(y_pre, color = 'red', linewidth = 3, label = 'predict')             
-#     line2, = plt.plot(y_test, color = 'blue', linewidth = 3, label = 'ground true')
-#     plt.legend(handles = [line1, line2])
-    # plt.savefig('Fe_r_plot.svg')                              # 儲存圖片
-    # plt.savefig('Fe_r_plot.png')
-    # plt.show()  
     #============================================================================
     hat_action = pd.DataFrame(hat_action,columns = ['Action'])
     # hat.rename(index = {"0":'Action'})
